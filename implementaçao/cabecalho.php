49d349c3d36f94c796b4b9adefe33d8df1e8e446
jeanrabello <jean.rabello.prog@gmail.com>
2021-06-13 16:46:58 -0300

    Versão 1.0

---
+++ implementaçao/cabecalho.php
@@ -0,0 +1,25 @@
+
+<div class="cabecalho__logo">
+    <a href=""><img src="./Img/Logo.png"/></a>
+</div>
+
+<ul class="cabecalho__menu">
+    <li><a href="">Mouses       </a> </li>
+    <li><a href="">Mousepads    </a> </li>
+    <li><a href="">Monitores    </a> </li>
+    <li><a href="">Teclados     </a> </li>
+    <li><a href="">Headphones   </a> </li>
+    <li><a href="">Headsets     </a> </li>
+    <li><a href="">Cadeiras     </a> </li>
+    <li><a href="">Mousepads    </a> </li>
+
+    <li><div class="menu__usuario">
+        <button class="dropbtn"><img src="./Img/usuario.svg" /></button>
+        <div class="menu__usuario-conteudo">
+            <a href="#">Meu Perfil       </a>
+            <a href="">Gerir Produtos    </a>
+            <a href="">Gerar Relatório   </a>
+            <a href="">Logout            </a>
+        </div>
+    </div></li>
+</ul>
---
+++ implementaçao/corpo.php
@@ -0,0 +1,61 @@
+<div class="Conteudo">
+    <div class="Conteudo__banner">
+        <img src="./Img/banner.jpg"/>
+    </div>
+
+    <div class="Conteudo__Produtos">
+        <div class="Conteudo__Produtos--Ofertas">
+            
+            <li class="Titulo--Ofertas">
+                <a>
+                    <img src="./Img/offer.png" width="25" height="25"/>
+                    Ofertas
+                </a>
+            </li>
+
+            <li class="Imagens--Ofertas">
+                <a href="">
+                    <img src="./Img/Produto.jpg"/>
+                </a>
+                <a href="">
+                    <img src="./Img/Produto.jpg"/>
+                </a>
+                <a href="">
+                    <img src="./Img/Produto.jpg"/>
+                </a>
+                <a href="">
+                    <img src="./Img/Produto.jpg"/>
+                </a>
+                <a href="">
+                    <img src="./Img/Produto.jpg"/>
+                </a>
+            </li>
+
+            <li class="Titulo--Mouses">
+                <a>
+                    Mouses
+                </a>
+            </li>
+
+            <li class="Imagens--Mouses">
+                <a href="">
+                    <img src="./Img/Produto.jpg" width="250" height="300"/>
+                </a>
+                <a href="">
+                    <img src="./Img/Produto.jpg" width="250" height="300"/>
+                </a>
+                <a href="">
+                    <img src="./Img/Produto.jpg" width="250" height="300"/>
+                </a>
+                <a href="">
+                    <img src="./Img/Produto.jpg" width="250" height="300"/>
+                </a>
+                <a href="">
+                    <img src="./Img/Produto.jpg" width="250" height="300"/>
+                </a>
+            </li>
+
+
+        </div>
+    </div>
+</div>
